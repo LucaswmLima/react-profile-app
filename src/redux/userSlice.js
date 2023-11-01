8c6c70c6c6f64d488d973437004df8db51de75b6
Lucas William <lucaswilliamml@gmail.com>
2023-10-31 22:08:08 -0300

    add redux

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -3,17 +3,17 @@ import { createSlice } from "@reduxjs/toolkit";
 export const userSlice = createSlice({
   name: "user",
   initialState: {
-      name: "Nayun",
-      email: "nayun@email.com",
-
+    name: "Nayun",
+    email: "nayun@email.com",
   },
   reducers: {
-    update: (state,action)=>{
-        state.name = action.payload.name;
-        state.email = action.payload.email;
-    }
+    update: (state, action) => {
+      state.name = action.payload.name;
+      state.email = action.payload.email;
+    },
+    remove: (state) => (state = {}),
   },
 });
 
-export const { update } = userSlice.actions;
-export default userSlice.reducer
+export const { update, remove } = userSlice.actions;
+export default userSlice.reducer;
